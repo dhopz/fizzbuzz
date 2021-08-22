972862c8f86051bfb79d913effcb4ec4805601be
David Houpapa <david_houpapa@hotmail.com>
2021-08-22 16:48:27 +0100

    finishing up the rspec function

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
-def fizzbuzz(number)
-    'fizz'
+def fizzbuzz(n)  
+    if n % 3 == 0 and n % 5 == 0
+        "fizzbuzz"
+    elsif n % 3 == 0
+        "fizz"
+    elsif n % 5 == 0
+        "buzz"
+    else
+        n 
+    end    
 end
+
